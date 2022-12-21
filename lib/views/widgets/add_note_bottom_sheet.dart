58d60dfe20cad805b2549bdbd4bc321a3bac485d
eshakelbadry <eshak.elbadry96@gmail.com>
2022-12-21 17:19:53 +0200

    adding content  text field and add buttom

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:keep_notes_app/constants.dart';
 
 import 'custom_text_field.dart';
 
@@ -11,8 +12,40 @@ class AddNoteBottomSheet extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Column(children: const [
         SizedBox(height: 32),
-        CustomTextField(),
+        CustomTextField(hint: 'title'),
+        SizedBox(height: 16),
+        CustomTextField(hint: 'content', maxLines: 5),
+        SizedBox(height: 32),
+        CustomButton()
       ]),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(
+          8,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
     );
   }
 }
